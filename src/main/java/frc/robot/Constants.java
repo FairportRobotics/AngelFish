39536fbb8c55def776afdc8e18c963f1e99b3c3e
Peter (Wrenyx) Wilcox <peternator87@gmail.com>
2023-01-24 19:36:34 -0500

    Add Gripper Subsystem

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,8 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -12,4 +14,10 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+    
+    // Gripper Pneumatics
+    public static final PneumaticsModuleType PCM = PneumaticsModuleType.CTREPCM;
+    public static final int PCM_GRIPPER_CLOSE = 2;
+    public static final int PCM_GRIPPER_OPEN = 3;
+}
