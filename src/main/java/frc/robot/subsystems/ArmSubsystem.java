490dbaa2718c9c2a62ecd707840769da8a9b5fc6
RealGrimmCreeper <jacksonsdistractions@gmail.com>
2023-02-02 19:55:48 -0500

    added code to arm

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -5,16 +5,32 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-// import com.ctre.phoenix.motorcontrol.can;
+import com.ctre.phoenix.motorcontrol.can;
 
 public class ArmSubsystem extends SubsystemBase {
   /** Creates a new ArmSubsystem. */
+  private AnalogInput shoulderAnalogInput;
   double speed = .5;
   public void set( double speed ) {
     
   }
-  public ArmSubsystem() {}
+  public ArmSubsystem() {
+    shoulderAnalogInput = new AnalogInput(); 
+  }
 
+  public void ArmMovePossition (){
+    //  for(int getArmPossition=1; 1<50;i++);
+    // 
+    // need range of voltage/ range of motion of Arm
+    // need exact voltage value of 3 or 4 possitons
+    // some sort of if statement will be used to 
+    // move the arm to a new possition
+    //
+  }
+
+  public getArmPossition (){
+    // .getvoltage shoulderAnalogInput;
+  }
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
@@ -24,4 +40,5 @@ public class ArmSubsystem extends SubsystemBase {
   public void simulationPeriodic() {
     // This method will be called once per scheduler run during simulation
   }
+  
 }
