105873c0ef767d6fdcd52e55242ee944f37e23ed
Peter (Wrenyx) Wilcox <peternator87@gmail.com>
2023-02-04 13:16:38 -0500

    comitt constents merj

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -36,7 +36,11 @@ public final class Constants {
 
     public static final double GO_TO_POSITION_ERROR = 0.3;
     public static final double GO_TO_VELOCITY_ERROR = 0.1;
+    
+    // Gripper Pneumatics
+    public static final PneumaticsModuleType PH = PneumaticsModuleType.REVPH;
     public static final int PH_GRIPPER_CLOSE = 2;
+    public static final int PH_GRIPPER_OPEN = 3;
     public static final int PH_CAN_ID = 18;
     // Controller
     public static final int DRIVER_CONTROLLER = 0;
