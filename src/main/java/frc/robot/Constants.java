5db03bb7eddfd67b0b7883a013729197b9978bd4
Wrenyx <51275040+Falafelnator87@users.noreply.github.com>
2023-02-04 11:41:13 -0500

    Initial gripper contributions (#2)
    * Add Gripper Subsystem
    
    * Initial framework with gripper example
    
    * Finished gripper sub/com, button is toggle
    
    * added a safety for gripper toggle
    
    * AAAAdded pneumatics hub and set it up
    
    * Add Gripper Subsystem
    
    * Initial framework with gripper example
    
    * Finished gripper sub/com, button is toggle
    
    * added a safety for gripper toggle
    
    * AAAAdded pneumatics hub and set it up
    
    * gripper changes from code revew
    
    * gripper changes from code review
    
    ---------
    
    Co-authored-by: Matt <mbzaner@gmail.com>
    Co-authored-by: Erik Andrews <erik.andrews@outlook.com>

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,8 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -12,4 +14,26 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+    
+    // Gripper Pneumatics
+    public static final PneumaticsModuleType PH = PneumaticsModuleType.REVPH;
+    public static final int PH_GRIPPER_CLOSE = 2;
+    public static final int PH_GRIPPER_OPEN = 3;
+    public static final int PH_CAN_ID = 18;
+    // Controller
+    public static final int OPERATOR_CONTROLLER = 1;
+
+    // Buttons
+    public static final int GRIPPER_TOGGLE = 0; // Talk to operator on if held
+    public static final int GRIPPER_SAFETY = 1; 
+    public static final int UNDERFINED_BUTTON_3 = 2; // Implement some sort of wrist reset
+    public static final int UNDERFINED_BUTTON_4 = 3;
+    public static final int UNDERFINED_BUTTON_5 = 4;
+    public static final int UNDERFINED_BUTTON_6 = 5;
+    public static final int UNDERFINED_BUTTON_7 = 6;
+    // add more if needed
+
+
+
+}
