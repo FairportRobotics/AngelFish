c4b6debce4b2608ed0efa30743b94e2479cb858c
Erik Andrews <erik.andrews@outlook.com>
2023-02-11 10:25:33 -0500

    Added color sensor checking

--- src/main/java/frc/robot/subsystems/GripperSubsystem.java
+++ src/main/java/frc/robot/subsystems/GripperSubsystem.java
@@ -1,5 +1,7 @@
 package frc.robot.subsystems;
 
+import javax.lang.model.util.ElementScanner14;
+
 import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
@@ -13,12 +15,41 @@ public class GripperSubsystem extends SubsystemBase{
 
     private DoubleSolenoid gripperSolenoid;
     private PneumaticHub ph;
+    private final int MIN_RED_CONE = 205;
+    private final int MAX_RED_CONE = 265;
+    private final int MIN_GREEN_CONE = 154;
+    private final int MAX_GREEN_CONE = 214;
+    private final int MIN_BLUE_CONE = 0;
+    private final int MAX_BLUE_CONE = 30;
+    private final int MIN_RED_CUBE = 123;
+    private final int MAX_RED_CUBE = 183;
+    private final int MIN_GREEN_CUBE = 32;
+    private final int MAX_GREEN_CUBE = 92;
+    private final int MIN_BLUE_CUBE = 151;
+    private final int MAX_BLUE_CUBE = 211;
 
     public GripperSubsystem() {
         // Added some stuff from askar to constants and changed the names, PH as well.
         ph = new PneumaticHub(Constants.PH_CAN_ID);
         gripperSolenoid = ph.makeDoubleSolenoid(Constants.PH_GRIPPER_OPEN, Constants.PH_GRIPPER_CLOSE);
         gripperSolenoid.set(Value.kForward);
+    }
+
+    public GamePiece checkColors(int r, int g,int b)
+    {
+      if(r >=MIN_RED_CONE && r <= MAX_RED_CONE && g >= MIN_GREEN_CONE && g <= MAX_GREEN_CONE && b >= MIN_BLUE_CONE && b <= MAX_BLUE_CONE)
+        return GamePiece.CONE;
+      else if(r >= MIN_RED_CUBE && r <= MAX_RED_CUBE && g >= MIN_GREEN_CUBE && g <= MAX_GREEN_CUBE && b >= MIN_BLUE_CUBE && b <= MAX_BLUE_CUBE)
+        return GamePiece.CUBE;
+      else
+        return GamePiece.NONE;
+    }
+
+    public enum GamePiece
+    {
+      NONE,
+      CONE,
+      CUBE
     }
 
     // Pistons that push/pull gripper claws -- toggles position
